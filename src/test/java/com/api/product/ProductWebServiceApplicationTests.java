fc3f1682081ff7e7b02984225e0c8c44e2512680
Celso Machado <celsobarrosmachado@gmail.com>
2023-08-15 10:41:38 +0100

    First Release

---
+++ src/test/java/com/api/product/ProductWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
